05e5a11a4053e9f1947c6e076c51a570f3e261f8
OJ <onkarjoshi296@gmail.com>
2024-04-02 10:26:09 +0530

    update search header component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -28,9 +28,9 @@ export default function HomeSearch() {
     <>
       <form
         onSubmit={handleSubmit}
-        className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'
+        className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-400 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'
       >
-        <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
+        <AiOutlineSearch className='text-xl text-gray-800 mr-3' />
         <input
           type='text'
           className='flex-grow focus:outline-none'
